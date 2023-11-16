8b21dea8305f5911dea838f65d9d58dc8a3c3e7b
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-17 00:19:25 +0100

    struttura terzo snack (C)

---
+++ jsnack/Snack C/script.js
@@ -0,0 +1,7 @@
+// Chiedi all'utente due numeri, uno piu' piccolo (due cifre) e uno piu' grande
+// (almeno quattro cifre): moltiplica il primo numero per 2 finche' non arriva
+//  ad essere maggiore del secondo numero.
+
+// richiesta di due numeri, rispettivamente uno da due e l'altro da 4 cifre
+// creare un loop dove si moltiplica solo il primo numero per 2
+// creare un IF + condizione secondo la quale il primo numero si moltiplica per 2 FINCHE non arriva a superare il secondo numero da 4 cifre
