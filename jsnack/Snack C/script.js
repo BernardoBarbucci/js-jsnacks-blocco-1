caabdabed5f15a05a40dc3c1140d2c9b90fee46e
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-17 00:44:26 +0100

    programma completo, implementazione del cycle for che moltiplica per 2 finche secondNumber non è maggiore di firstNumber. Special thanks goes to MDN!

--- jsnack/Snack C/script.js
+++ jsnack/Snack C/script.js
@@ -9,9 +9,15 @@ let secondNumber = prompt('Inserisci un numero di 4 cifre: ');
 // verifica che i numeri siano validi e soprattutto delle cifre richieste
 if (!isNaN(parseInt(firstNumber)) && firstNumber.length === 2 &&
     !isNaN(parseInt(secondNumber)) && secondNumber.length === 4) {
-        console.log('Hai inserito due numeri validi:', firstNumber, secondNumber); 
+    // creare un loop dove si moltiplica solo il primo numero per 2
+    // prpblema risolto con ciclo for. su mdn cè scirtto esplicitamente di mettere il ; prima della prima variabile, infatti senza il programma non andava
+    for (; firstNumber <= secondNumber; ) {
+        firstNumber *= 2;
+    }
+
+        console.log('Per superare il secondo numero, il primo numero è stato moltiplicato per 2 fino ad arrivare a: ', firstNumber); 
     } else {
         console.log('I numeri che hai inserito non sono della lunghezza richiesta, il primo deve essere di 2 cifre ed il secondo di 4!')
     }
-// creare un loop dove si moltiplica solo il primo numero per 2
-// creare un IF + condizione secondo la quale il primo numero si moltiplica per 2 FINCHE non arriva a superare il secondo numero da 4 cifre
+
+
