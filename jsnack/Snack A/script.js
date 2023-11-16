559fef766feb34736ca0642d6009df42f0ea5bbf
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-16 17:40:23 +0100

    inserito result fuori dal cycle per vedere in console i numeri dispari inseriti

--- jsnack/Snack A/script.js
+++ jsnack/Snack A/script.js
@@ -16,6 +16,7 @@ for (i = 0; i <= 5; i++) {
     } else {
         console.log('Non è dispari, ma prova ancora fino a 6 volte');
     }
-    // console.log('Hai inserito:', userInput);
-}
 
+}
+// result in console dei numeri dispari inseriti nell'array
+console.log('Numeri dispari che hai inserito: ', oddNumbers);
