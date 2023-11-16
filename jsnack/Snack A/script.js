f241e43f9a245d1382bb724466bcfff1744826b3
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-16 17:17:08 +0100

    struttura snacks + inizio snack A (array vuoto, let per chiedere il numero + cycle for per richieder 6 volte un numero all'utente)

---
+++ jsnack/Snack A/script.js
@@ -0,0 +1,12 @@
+// Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
+
+// // arry vuoto
+const oddNumbers [''];
+
+// richiesta all'utente
+let userNumber = prompt('Inserisci qua il tuo numero: ');
+console.log('Hai inserito:', userInput);
+
+// ciclo for per chiedere 6 volte il numero all'utente
+for (i = 0; )
+
