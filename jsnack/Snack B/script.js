dcbdb6b810201c3d471df3dd24fc17e5bec45788
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-17 00:06:50 +0100

    aggiunto array + .split insieme cosi da creare un loop che somma tutti i numeri singolarmente

--- jsnack/Snack B/script.js
+++ jsnack/Snack B/script.js
@@ -9,16 +9,15 @@ let  numberCheck = parseInt(userNumber);
 // verifica che sia un numero a 7 e solo 7 cifre (===)
 if (!isNaN(numberCheck) && userNumber.length === 7) {
     // dentro if metto un cycle for per sommare i numeri che compongono il numero a 7 cifre,
-    // sempre se è un unmero a 7 cifre
-        // step 1 = creo una variabile
+    // sempre se è un numero a 7 cifre
+        // step-1 = creo una variabile
         let sum = 0;
-        // step-2 = faccio la somma con un cycle for
-        for (let = if; i < userNumber.length; i++) {
-            sum += (userNumber);
+        // step-2 = faccio la somma con un cycle for e creo quindi un loop per sommare i singoli numeri -> uso .split
+        for (let = i; i < userNumber.length; i++) {
+            sum += parseInt(userNumber[i]);
         }
-
     
-    console.log('hai inserito un numero a 7 cifre, grande!');
+    console.log('hai inserito un numero a 7 cifre e la somma di tutti i numeri è: ', sum);
 } else {
     console.log('Il numero che hai inserito non è di 7 cifre, ritenta dai');
 }
