caf2c9debc2cecb2f0b567ddcd33772b873eeca3
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-16 18:16:41 +0100

    struttura js con array per inserire il numero (probabilmente da rimuovere) e let prompt per richiedere il numero

---
+++ jsnack/Snack B/script.js
@@ -0,0 +1,6 @@
+// Chiedi un numero di 7 cifre all'utente e calcola la somma di tutte le cifre che compongono il numero.
+
+// array per inserire i numeri?
+const userSum = [];
+
+let userQuestion = prompt('Inserisci qua un numero a 7 cifre: ');
