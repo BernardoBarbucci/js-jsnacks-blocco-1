e4eab94b5b456a127c6fb15d368d60d132ad2ee4
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-16 17:35:32 +0100

    verifica che il numero sia dispari ma come result ho sempre il console.log di else

--- jsnack/Snack A/script.js
+++ jsnack/Snack A/script.js
@@ -1,12 +1,21 @@
 // Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
 
 // // arry vuoto
-const oddNumbers [''];
+const oddNumbers = [];
 
 // ciclo for per chiedere 6 volte il numero all'utente
-for (i = 0; i <= 6; i++) {
+for (i = 0; i <= 5; i++) {
     // richiesta all'utente
     let userNumber = prompt('Inserisci qua il tuo numero: ');
-console.log('Hai inserito:', userInput);
+    
+    // verifica se il numero è dispari, se si allora inseriscilo nell'array
+        // Se number è 5, allora 5 % 2 è 1, e 1 !== 0 è true
+        // Se number è 8, allora 8 % 2 è 0, e 0 !== 0 è false
+    if (userNumber % 2 !==0) {
+        oddNumbers.push(userNumber);
+    } else {
+        console.log('Non è dispari, ma prova ancora fino a 6 volte');
+    }
+    // console.log('Hai inserito:', userInput);
 }
 
