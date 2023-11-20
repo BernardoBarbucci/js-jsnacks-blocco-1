b88d2c3a6c03800af9eb7fbe134e3255f7c149c7
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-20 11:45:32 +0100

    aggiunto prompt e richiamata la function all'esterno

--- snack-7/script.js
+++ snack-7/script.js
@@ -2,15 +2,17 @@
 // Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
-function twoWords (wordOne, wordTwo) {
-    let wordOnee = ('Inserisci la prima parola');
-    let wordTwo = ('Inserisci la seconda parola');
+function twoWordsLenght (wordOne, wordTwo) {
+    let wordOnee = prompt('Inserisci la prima parola');
+    let wordTwo = prompt('Inserisci la seconda parola');
     if (wordOne.length === wordTwo.length) {
         console.log("Le parole hanno la stessa lunghezza.");
     } else {
         console.log("Le parole hanno lunghezze diverse.");
     }
 }
+
+twoWordsLenght();
 
 
 
