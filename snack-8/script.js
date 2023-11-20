68d9ba39543cf5debb2238bb4b1494a8fad869b1
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-20 12:32:50 +0100

    funzione completata

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,19 +1,18 @@
 // Crea una funzione che accetta due numeri, se il primo e' divisibile per il secondo,
 // allora ritorna vero, altrimenti ritorna falso. 
 // Micro bonus: se sono uguali ritorna il numero.
-
-
-function checkDivisibility() {
     let numberOne = prompt('Inserisci il primo numero');
     let numberTwo = prompt('Inserisci il secondo numero');
 
     numberOne = parseFloat(numberOne);
     numberTwo = parseFloat(numberTwo);
 
+function checkDivisibility() {
+
     if (numberOne !== 0 && numberOne % numberTwo === 0) {
-        console.log(numberOne, numberTwo)
+        return true;
     } else {
-        console.log('Il primo numero non è divisibile per il secondo');
+        return false;
     }
 }
 
---
+++ snack-9/script.js
@@ -0,0 +1,6 @@
+// Crea una funzione che accetta due elementi del dom, uno e' il parent e
+// l'altro e' un elemento che voglio sia aggiunto al parent come figlio.
+
+function addElFirstToElTwo(elementFirst, elementTwo) {
+    elementFirst.appendChild(elementTwo);
+}
