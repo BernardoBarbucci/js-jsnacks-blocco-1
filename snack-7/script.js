321f6425df393844eddcf16e9b57a89aaa8f710e
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-20 12:07:57 +0100

    snack completato e corretto in base all'esempio di Ricca

--- snack-7/script.js
+++ snack-7/script.js
@@ -3,28 +3,44 @@
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
 
+
+
+// function twoWordsLength (wordOne, wordTwo) {
+
+//     if (wordOne.length === wordTwo.length) {
+//         console.log("Le parole hanno la stessa lunghezza.");
+//         console.log('Prima parola: ', wordOne);
+//         console.log('Seconda parola: ', wordTwo);
+//     } else {
+//         console.log("Le parole hanno lunghezze diverse.");
+
+//         if (wordOne.length > wordTwo.length) {
+//             console.log('Parola piu lunga:', wordOne);
+//         } else {
+//             console.log('Parola piu lunga: ', wordTwo);
+//         }
+//     }
+// }
+
+// twoWordsLength();
+
+// soluzione di Ricca
 let wordOne = prompt('Inserisci la prima parola');
 let wordTwo = prompt('Inserisci la seconda parola');
 
-
-function twoWordsLength (wordOne, wordTwo) {
-
-    if (wordOne.length === wordTwo.length) {
-        console.log("Le parole hanno la stessa lunghezza.");
-        console.log('Prima parola: ', wordOne);
-        console.log('Seconda parola: ', wordTwo);
-    } else {
-        console.log("Le parole hanno lunghezze diverse.");
-
-        if (wordOne.length > wordTwo.length) {
-            console.log('Parola piu lunga:', wordOne);
-        } else {
-            console.log('Parola piu lunga: ', wordTwo);
-        }
-    }
+if (twoWordsLength (wordOne, wordTwo) ) {
+    console.log(wordOne , wordTwo) 
+} else if (wordOne.length > wordTwo.length) {
+    console.log(wordOne);
+} else {
+    console.log(wordTwo);
 }
 
-twoWordsLength();
 
-
-
+function twoWordsLength (wordOne, wordTwo) {
+    if (wordOne.length === wordTwo.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
