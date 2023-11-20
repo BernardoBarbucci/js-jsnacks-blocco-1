db8b70bb4437fc76bf1569438f1a997deed3be6b
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-20 12:23:17 +0100

    aggiornato e messo dentro ad una funzione

--- snack-8/script.js
+++ snack-8/script.js
@@ -3,16 +3,18 @@
 // Micro bonus: se sono uguali ritorna il numero.
 
 
+function checkDivisibility() {
+    let numberOne = prompt('Inserisci il primo numero');
+    let numberTwo = prompt('Inserisci il secondo numero');
 
-let numberOne = prompt('Inserisci il primo numero');
-let numberTwo = prompt('Inserisci il secondo numero');
+    numberOne = parseFloat(numberOne);
+    numberTwo = parseFloat(numberTwo);
 
-numberOne = parseFloat(numberOne);
-numberTwo = parseFloat(numberTwo);
-
-if (numberOne !== 0 && numberOne % numberTwo === 0) {
-    console.log(numberOne, numberTwo)
-} else {
-    console.log('Il primo numero non è divisibile per il secondo');
+    if (numberOne !== 0 && numberOne % numberTwo === 0) {
+        console.log(numberOne, numberTwo)
+    } else {
+        console.log('Il primo numero non è divisibile per il secondo');
+    }
 }
 
+checkDivisibility();
