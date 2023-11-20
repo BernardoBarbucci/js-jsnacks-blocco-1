1554c258f71445dbd587a9de1dbd75d7b0d64ca5
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-20 11:39:25 +0100

    richiesta parole nella funzione

---
+++ snack-7/script.js
@@ -0,0 +1,11 @@
+// Dare la possibilità di inserire due parole. 
+// Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
+// Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+function twoWords (wordOne, wordTwo) {
+    let wordOnee = ('Inserisci la prima parola');
+    let wordTwo = ('Inserisci la seconda parola');
+}
+
+
+
