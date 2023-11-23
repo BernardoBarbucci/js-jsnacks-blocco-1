f5feecd3623c31ed84a3c7466c22cdab2f36db97
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-23 12:25:20 +0100

    struttura html, css

---
+++ snack-10/script.js
@@ -0,0 +1,7 @@
+// chiedo all'utente un numero e genero attraverso una funzione un numero di quadrati equivalente al numero inserito dall'utente nel DOMException, in qualsiasi forma
+
+let userNumber = parseInt(prompt('Inserisci un numero'));
+
+function createSquares () {
+
+}
