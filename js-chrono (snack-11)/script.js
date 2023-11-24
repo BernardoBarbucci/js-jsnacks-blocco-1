908e34a5507643dd49b4caa9e4851a1e089074ae
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-24 12:31:56 +0100

    creazione zucchine e filling delle infos

--- js-chrono (snack-11)/script.js
+++ js-chrono (snack-11)/script.js
@@ -1,2 +1,31 @@
 // Cliccando su un pulsante, viene avviato un cronometro. 
 // Per fermare il cronometro, bisogna cliccare su un secondo pulsante
+
+// prendo gli el dall'id
+const startButton = document.getElementById('startButton');
+const endButton = document.getElementById('endButton');
+
+let timer; // Variabile per memorizzare il timer
+let startTime; // Variabile per memorizzare il tempo di inizio
+
+startButton.addEventListener('click', startTimer);
+endButton.addEventListener('click', endTimer);
+
+function startTimer() {
+    if (!timer) {
+        startTime = Date.now(); // Memorizza il tempo di inizio
+        timer = setInterval(() => {
+            const currentTime = Date.now();
+            const elapsedTime = (currentTime - startTime) / 1000; // Calcola il tempo trascorso in secondi
+            console.log('Elapsed time:', elapsedTime, 'seconds');
+        }, 1000);
+    }
+}
+
+function endTimer() {
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+        console.log('Timer stopped');
+    }
+}
---
+++ snack-12 (zucchine)/script.js
@@ -0,0 +1,58 @@
+// Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+const zucchina = {
+    varietà: 'romanesca',
+    peso: '3 kg',
+    lunghezza: '1m'
+};
+
+const zucchina = {
+    varietà: 'fiorentina',
+    peso: '2.5 kg',
+    lunghezza: '.75m'
+};
+
+const zucchina = {
+    varietà: 'napoletana',
+    peso: '.8 kg',
+    lunghezza: '.2 m'
+};
+
+const zucchina = {
+    varietà: 'tonde',
+    peso: '3 kg',
+    lunghezza: '1 metro'
+};
+const zucchina = {
+    varietà: 'romanesca',
+    peso: '3 kg',
+    lunghezza: '1 metro'
+};
+const zucchina = {
+    varietà: 'romanesca',
+    peso: '3 kg',
+    lunghezza: '1 metro'
+};
+const zucchina = {
+    varietà: 'romanesca',
+    peso: '3 kg',
+    lunghezza: '1 metro'
+};
+const zucchina = {
+    varietà: 'romanesca',
+    peso: '3 kg',
+    lunghezza: '1 metro'
+};
+const zucchina = {
+    varietà: 'romanesca',
+    peso: '3 kg',
+    lunghezza: '1 metro'
+};
+const zucchina = {
+    varietà: 'romanesca',
+    peso: '3 kg',
+    lunghezza: '1 metro'
+};
+
+
+console.log(zucchina); 
